582f28eaa3b5f75b610dfe7760038eb861097dd7
Guilherme Iram <guilherme.iram@gmail.com>
2022-07-10 17:41:11 -0300

    Variaveis numericas e textuais

---
+++ texto/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ola, texto!");
+
+        char letra = 'a';
+        Console.WriteLine("Caracter: " + letra);
+        letra = (char) 65; // convertendo int em char
+        Console.WriteLine("Caracter: " + letra);
+        letra = (char) (65 + 1);
+        Console.WriteLine("Caracter: " + letra);
+        letra += letra; // soma a parte inteira dos caracteres
+        Console.WriteLine("Caracter: " + letra);
+
+        Console.WriteLine("Trabalhando com strings ...");
+        string primeiraFrase = "Csharp é uma linguagem de programação";
+        Console.WriteLine(primeiraFrase + " interessante\n");
+        
+        
+        string vazia = "";
+        Console.WriteLine("String vazia: " + vazia);
+        vazia += "conteudo";
+        Console.WriteLine("String vazia: " + vazia);
+
+        Console.WriteLine("Uso do @ para listagem em strings: ");
+        string lista = @"Lista de compras
+banana
+    maca
+        torta
+            cebola";
+
+        Console.WriteLine(lista + '\n');
+
+        Console.WriteLine("Mostrando que o operador \'+\' concatena diferentes tipos numa string ...");
+        string saudacao = "Olá, meu nome é ";
+        string nome = "Carlos ";
+        string continuacao = "e minha idade é ";
+        int idade = 27;
+        Console.WriteLine(saudacao + nome + continuacao + idade);
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
---
+++ variaveis numericas/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace variaveis
+{
+    public class program
+    {
+        static void Main(string[] args){
+
+        Console.WriteLine("Hello, variaveis!");
+        int idade; // usando int
+        idade = 21;
+        
+        Console.WriteLine("Minha idade eh " + idade); // Nao quebra a linha
+        Console.Write("Sua idade? "); // quebra a linha
+        string input = Console.ReadLine(); // recebe um input
+        int suaIdade = Convert.ToInt16(input); // converte o input str para int16
+        Console.WriteLine("Sua idade eh " + suaIdade + "\n"); // exemplo de concatenacao em C#
+
+        
+        Console.Write("Digite n1: ");
+        int n1 = Convert.ToInt16(Console.ReadLine()); // recebendo um input direto n1
+        Console.Write("Digite n2: ");
+        int n2 = Convert.ToInt16(Console.ReadLine()); // recebendo um input direto n2
+        int res;
+
+        Console.WriteLine("\nOperações inteiras: ");
+        res = n1 + n2;
+        Console.WriteLine("" + n1 + " + " + n2 + " = " + res);
+        res = n1 - n2;
+        Console.WriteLine("" + n1 + " - " + n2 + " = " + res);
+        res = n1 * n2;
+        Console.WriteLine("" + n1 + " * " + n2 + " = " + res);
+        res = n1 / n2;
+        Console.WriteLine("" + n1 + " / " + n2 + " = " + res);
+        res = n1 % n2;
+        Console.WriteLine("" + n1 + " % " + n2 + " = " + res);
+        
+        
+        
+        double n3 = 9;
+        double n4 = 4;
+        double res_d; //convencao por efeito colateral (8 / 3) == 2, (8.0 / 3) == 2.6666...
+        Console.WriteLine("Operacoes double: ");
+        res_d = n3 + n4;
+        Console.WriteLine("" + n3 + " + " + n4 + " = " + res_d);
+        res_d = n3 - n4;
+        Console.WriteLine("" + n3 + " - " + n4 + " = " + res_d);
+        res_d = n3 * n4;
+        Console.WriteLine("" + n3 + " * " + n4 + " = " + res_d);
+        res_d = n3 / n4;
+        Console.WriteLine("" + n3 + " / " + n4 + " = " + res_d);
+        res_d = n3 % n4;
+        Console.WriteLine("" + n3 + " % " + n4 + " = " + res_d);
+
+
+        int n5_double = 5;
+        Console.WriteLine("\nConvertendo um int \'" + n5_double + "\' em double ...");
+        double n5_int = (double) n5_double;
+        Console.WriteLine("Precisei criar uma nova variável pra isso");
+        Console.WriteLine("Agora temos " + n5_double + ", prova: " + n5_double + " / 2.0 = " + (n5_double / 2.0));
+        Console.WriteLine("Se eu tentasse fazer \'5 / 2\' receberia \'2\' ao inves de \'2,5\'");
+
+        Console.WriteLine("\nAperte enter para fechar ...");
+        Console.ReadLine();
+
+
+        }
+
+    }
+
+}
+
